d414ac8fb9ff015c36b397639894f939387768e5
Briggendudex <sigurd@sivertsens.net>
2023-03-12 16:16:47 +0100

    new test

--- main.tf
+++ main.tf
@@ -1,22 +1,13 @@
-## Use the AzureRM provider
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-}
-
-# Create the resourcegroup
+# Create the resource group
 resource "azurerm_resource_group" "appgw-resourcegroup" {
   name     = var.resource_group_name
   location = var.location
-
 }
+
 # Create a subnet for the Application Gateway
 resource "azurerm_subnet" "app_gateway_subnet" {
   name                 = var.subnet_name
-  resource_group_name  = var.vnet-rg-name
+  resource_group_name  = var.vnet_rg_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = [var.subnet_address_prefix]
 }
@@ -27,49 +18,44 @@ resource "azurerm_public_ip" "app_gateway_public_ip" {
   resource_group_name = var.resource_group_name
   location            = var.location
   allocation_method   = var.public_ip_allocation_method
-  sku                 = "Standard"
+  sku                 = var.public_ip_sku
 }
 
 # Create a backend pool for the Application Gateway
-# resource "azurerm_lb_backend_address_pool" "backend_pool" {
-#   name                = var.backend_pool_name
-#   resource_group_name = var.resource_group_name
-#   loadbalancer_id     = module.app_gateway.app_gateway_id
-#   backend_ips = var.backend_ips
-#   key_vault_url = var.key_vault_url
-#  }
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  name                = var.backend_pool_name
+  resource_group_name = var.resource_group_name
+  loadbalancer_id     = module.app_gateway.app_gateway_id
+  backend_ips         = var.backend_ips
+  key_vault_url       = var.key_vault_url
+  count               = var.create_backend_pool ? 1 : 0
+}
 
-# # Create the Application Gateway
+# Create the Application Gateway
 resource "azurerm_application_gateway" "app_gateway" {
-  name                = "myappgateway"
+  name                = var.app_gateway_name
   resource_group_name = var.resource_group_name
   location            = var.location
-
   sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
+    name     = var.app_gateway_sku_name
+    tier     = var.app_gateway_sku_tier
+    capacity = var.app_gateway_capacity
   }
   gateway_ip_configuration {
-    name      = "app_gateway_ip_configuration"
+    name      = var.gateway_ip_configuration_name
     subnet_id = azurerm_subnet.app_gateway_subnet.id
   }
   frontend_port {
     name = var.frontend_port_name
-    port = 80
+    port = var.frontend_port
   }
-  # frontend_port {
-  #   name = "app_gateway_frontend_port_https"
-  #   port = 443
-  # }
   frontend_ip_configuration {
     name                 = var.frontend_ip_configuration_name
     public_ip_address_id = azurerm_public_ip.app_gateway_public_ip.id
   }
   backend_address_pool {
-    name = var.backend_address_pool_name
+    name = var.backend_pool_name
   }
-
   backend_http_settings {
     name                  = var.backend_http_settings_name
     cookie_based_affinity = "Disabled"
@@ -78,45 +64,44 @@ resource "azurerm_application_gateway" "app_gateway" {
     protocol              = "Http"
     request_timeout       = 60
   }
-
   http_listener {
     name                           = var.http_listener_name
     frontend_ip_configuration_name = var.frontend_ip_configuration_name
     frontend_port_name             = var.frontend_port_name
-    protocol                       = "Http"
+    protocol                       = var.listener_protocol
   }
-
+  https_listener {
+    name                            = var.https_listener_name
+    frontend_ip_configuration_name  = var.frontend_ip_configuration_name
+    frontend_port_name              = var.frontend_port_name
+    protocol                        = var.listener_protocol
+    ssl_certificate_name            = var.ssl_certificate
+    require_server_name_indication  = true
+    count                           = var.create_https_listener ? 1 : 0
+  }
   request_routing_rule {
     name                       = var.request_routing_rule_name
     rule_type                  = "Basic"
     http_listener_name         = var.http_listener_name
-    backend_address_pool_name  = var.backend_address_pool_name
+    backend_address_pool_name  = var.backend_pool_name
     backend_http_settings_name = var.backend_http_settings_name
     priority                   = 1
   }
 }
 
+# Define output variables
+output "app_gateway_id" {
+  value = azurerm_application_gateway.app_gateway.id
+}
 
-# Create an HTTP listener for the Application Gateway
+output "app_gateway_public_ip_id" {
+  value = azurerm_public_ip.app_gateway_public_ip
+}
 
+output "app_gateway_public_ip_address" {
+value = azurerm_public_ip.app_gateway_public_ip.ip_address
+}
 
-# http_listener {
-#     name                           = var.https_listener_name
-#     frontend_ip_configuration_name = "app_gateway_frontend_ip"
-#     frontend_port_name             = "app_gateway_frontend_port_http"
-#     protocol                       = "Http"
-#   }
-
-#
-# Create an HTTPS listener for the Application Gateway
-
-# resource "azurerm_application_gateway_https_listener" "https_listener" {
-#   name                           = var.https_listener_name
-#   resource_group_name            = var.resource_group_name
-#   application_gateway_name       = azurerm_application_gateway.app_gateway.name
-#   frontend_ip_configuration_name = "app_gateway_frontend_ip"
-#   frontend_port_name             = "app_gateway_frontend_port_https"
-#   protocol                       = "Https"
-#   ssl_certificate                = base64decode(data.azurerm_key_vault_secret.ssl_cert.value)
-#   require_server_name_indication = true
-# }
+output "app_gateway_backend_pool_id" {
+value = azurerm_lb_backend_address_pool.backend_pool.id
+}
