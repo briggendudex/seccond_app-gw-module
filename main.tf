d16209fabaa5dc6094fc93be094323c9b3b48d5e
Briggendudex <sigurd@sivertsens.net>
2023-03-13 19:47:51 +0100

    new test

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_public_ip" "app_gateway_public_ip" {
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
   name                = var.backend_pool_name
   #resource_group_name = azurerm_resource_group.name
-  loadbalancer_id     = azurerm_application_gateway.app_gateway.id
+  loadbalancer_id     = azurerm_application_gateway.app_gateway.id  #azurerm_application_gateway.app_gateway.id
   #backend_ips         = var.backend_ips
   #key_vault_url       = var.key_vault_url
   #key_vault_id          = var.key_vault_id
@@ -71,7 +71,7 @@ resource "azurerm_application_gateway" "app_gateway" {
   #   frontend_port_name             = var.frontend_port_name
   #   protocol                       = var.listener_protocol
   # }
-  http_listener {
+  https_listener {
     name                            = var.https_listener_name
     frontend_ip_configuration_name  = var.frontend_ip_configuration_name
     frontend_port_name              = var.frontend_port_name
